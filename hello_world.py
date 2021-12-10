b0a4c477834b06b22a55d9c9cc29c3c102705df4
carlos crispin <julian234@gmail.com>
2021-12-10 00:21:59 -0500

    crea diccionario ANIMALS

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,17 @@
 import math, random
 
 
+
+
 print('hello world')
 
 num = random.randint(1,10)
 print(f'random integer = {num}')
+
+ANIMALS = {'antonia':'gato', 'luna':'perro'}
+
+print(ANIMALS)
+
 
 
 
